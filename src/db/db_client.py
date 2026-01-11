97789e1122a6862df568b417a466031e3b47fbfd
Dafloresdiaz <dafloresdiaz@gmail.com>
2026-01-10 20:52:16 -0600

    feat: Change hwo to make the connection to render redis database and for local redis

--- src/db/db_client.py
+++ src/db/db_client.py
@@ -1,15 +1,27 @@
-import redis
 import logging
+import redis
+import os
 
+redis_url = os.getenv("REDIS_URL")
+
+logging.basicConfig(level=logging.INFO)
 class DBClient:
     def __init__(self):
-        try:
-            self.redis_client = redis.Redis(
-                host='redis',
-                port=6379,
-                db=0,
-                decode_responses=True
-            )
-        except redis.RedisError as e:
-            logging.error(f"Redis connection error: {e}")
-            raise Exception(f"Error: {e}")
+        if redis_url:
+            try :
+                self.redis_client = redis.from_url(
+                    redis_url, decode_responses=True
+                )
+            except redis.RedisError as e:
+                logging.error(f"Redis connection error from Render server: {e}")
+        else:
+            try:
+                self.redis_client = redis.Redis(
+                    host='redis',
+                    port=6379,
+                    db=0,
+                    decode_responses=True
+                )
+            except redis.RedisError as e:
+                logging.error(f"Redis connection error: {e}")
+                raise Exception(f"Error: {e}")
